4e946b20dfdb734f4bf23932f24b4b9823dfedf8
bennett1993 <bennett.furman@gmail.com>
2023-02-28 17:05:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,35 @@
-import pandas as pd
-pd.set_option('display.max_columns', 500)
-from pathlib import Path
-import os
-import glob
-from IPython.core.interactiveshell import InteractiveShell
-InteractiveShell.ast_node_interactivity = "all"
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set(font_scale=1.5)
-import nbconvert
-import numpy as np
-import scipy
-import statsmodels.formula.api as smf
-import statsmodels.api as sm
-import warnings
-warnings.filterwarnings('ignore')
 import streamlit as st
-from datetime import date as dt
-import nltk
-nltk.download('stopwords')
-nltk.download('wordnet')
-nltk.download('omw-1.4')
-from nltk.corpus import stopwords
-from nltk.stem.wordnet import WordNetLemmatizer
-import string
+
+@st.cache
+def import_modules():
+    import pandas as pd
+    pd.set_option('display.max_columns', 500)
+    from pathlib import Path
+    import os
+    import glob
+    from IPython.core.interactiveshell import InteractiveShell
+    InteractiveShell.ast_node_interactivity = "all"
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    sns.set(font_scale=1.5)
+    import nbconvert
+    import numpy as np
+    import scipy
+    import statsmodels.formula.api as smf
+    import statsmodels.api as sm
+    import warnings
+    warnings.filterwarnings('ignore')
+    #import streamlit as st
+    from datetime import date as dt
+    import nltk
+    nltk.download('stopwords')
+    nltk.download('wordnet')
+    nltk.download('omw-1.4')
+    from nltk.corpus import stopwords
+    from nltk.stem.wordnet import WordNetLemmatizer
+    import string
+
+import_modules()
 
 st.set_page_config(layout="centered")
 
