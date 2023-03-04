abc06be454ef93458ed0af006c3809c137e25db9
bennett1993 <bennett.furman@gmail.com>
2023-03-03 23:18:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -113,13 +113,16 @@ with dataset:
     else:
         st.write('Please upload a csv file with the name additional_notes.csv, new_resulting_factors.csv, or tbi_incident.csv')        
         
+
 with eda:
     st.header('Exploratory Data Analysis')
     
+    first_chart = st.slider('Please choose the number of factors that you would like to see', min_value=1, max_value=20, value=10, step=1)
+    
     fig = plt.figure(figsize=(16, 8))
-    ax = sns.countplot(x="factor", data=new_resulting_factors, order=pd.value_counts(new_resulting_factors['factor']).iloc[:10].index)
+    ax = sns.countplot(x="factor", data=new_resulting_factors, order=pd.value_counts(new_resulting_factors['factor']).iloc[:first_chart].index)
     plt.xticks(rotation=90)
-    plt.title('Top Ten Factor Frequencies')
+    plt.title('Top f"{}" Factor Frequencies')
     
     for p in ax.patches:
         height = p.get_height()
@@ -129,6 +132,8 @@ with eda:
         ha = 'center')
  
     st.pyplot(fig)
+    
+    second_chart = st.slider('Please choose whether you would like the chart in ascending or descending order')
     
     fig = plt.figure(figsize=(16, 8))
     ax = sns.countplot(x='subcategory', data = new_resulting_factors, order=new_resulting_factors['subcategory'].value_counts(ascending=False).index)
@@ -173,125 +178,4 @@ with eda:
     
     
 with preprocessing:
-    today = dt.today()
-    patient_attributes = pd.merge(left=patient_info, right = symptom_details, left_on="patient_id", right_on="patient_id") 
-
-    patient_attributes['date_of_birth_clean'] = pd.to_datetime(np.where(pd.to_datetime(patient_attributes['date_of_birth']).dt.year > today.year, 
-                                                        (pd.to_datetime(patient_attributes['date_of_birth']).dt.year - 100).astype(str) + '-' + pd.to_datetime(patient_attributes['date_of_birth']).dt.month.astype(str) + '-' + pd.to_datetime(patient_attributes['date_of_birth']).dt.day.astype(str), 
-                                                        patient_attributes['date_of_birth']))
-
-    patient_attributes['age'] =  (pd.to_datetime(today) - pd.to_datetime(patient_attributes['date_of_birth_clean']))
-    patient_attributes['age'] = round((patient_attributes['age'].astype(str).str.split(' ',expand=True)[0].astype(int))/365.25,2)
     
-    patient_attributes = patient_attributes[['patient_id', 'age', 'gender', 'patient_type', 'id', 'details']]
-
-    patient_attributes_notes = pd.merge(left=additional_notes, right = patient_attributes, left_on="patient_id", right_on="patient_id") 
-    patient_attributes_notes = patient_attributes_notes[['id_x', 'patient_id', 'note', 'logged_at', 'age', 'gender', 'patient_type', 'details','additional_notes_date']]
-
-    master_data = pd.merge(left=new_resulting_factors, right = patient_attributes_notes, left_on=["symptom_date", "patient_about_id"], right_on=["additional_notes_date", "patient_id"]) 
-    master_data = master_data[['patient_id', 'patient_type', 'age', 'gender',  'details', 
-                            'had_symptom', 'severity', 'description', 'factor', 'category', 'subcategory',
-                            'logged_at_x',  'note']]
-    # Fill Nas
-    master_data['had_symptom'] = master_data['had_symptom'].fillna('False')
-    master_data['description'] = master_data['description'].fillna('False')
-    master_data['factor'] = master_data['factor'].fillna('None')
-    master_data['category'] = master_data['category'].fillna('None')
-    master_data['subcategory'] = master_data['subcategory'].fillna('None')
-    master_data['note'] = master_data['note'].fillna('None')
-    master_data['severity'] = master_data['severity'].fillna(master_data['severity'].mean())
-    master_data = master_data.rename(columns={'logged_at_x':'log_time'})  
-    master_data['agg_patient'] = master_data['patient_type'] + master_data['factor'] + master_data['subcategory']
-
-    stop = set(stopwords.words('english'))
-    exclude = set(string.punctuation)
-    lemma = WordNetLemmatizer()
-    
-    def clean(doc):
-        stop_free = " ".join([i for i in doc.lower().split() if i not in stop])
-        punc_free = ''.join(ch for ch in stop_free if ch not in exclude)
-        normalized = " ".join(lemma.lemmatize(word) for word in punc_free.split())
-        return normalized
-
-    master_data.note_clean = master_data.note.apply(lambda x: x.encode('ascii', 'ignore').decode('ascii'))
-
-
-    doc_clean = [clean(doc).split() for doc in master_data.note]  
-
-    master_data.note = doc_clean
-    
-'''with models:
-    st.header('BERT Model')
-
-    word_bank = list()
-    row = list()
-
-    kw_model = KeyBERT()
-
-    t = -1
-
-    for i in range(len(master_data['note'])):
-        
-        word_bank.append(kw_model.extract_keywords(master_data['note'][i]))
-        
-        t = t + 1
-        
-        row.append(t)
-    
-    
-    word_bank2 = word_bank.copy()
-    key_word_dict = {'index':row,'key_words':word_bank2,'key_word_1':None,'key_word_2':None,'key_word_3':None,'key_word_4':None,'key_word_5':None}
-    key_word_dataframe = pd.DataFrame(key_word_dict)
-
-    for i in range(len(key_word_dataframe)):
-        try: 
-            key_word_dataframe['key_word_1'][i] = key_word_dataframe['key_words'][i][0][0][0]
-            key_word_dataframe['key_word_2'][i] = key_word_dataframe['key_words'][i][1][0][0]
-            key_word_dataframe['key_word_3'][i] = key_word_dataframe['key_words'][i][2][0][0]
-            key_word_dataframe['key_word_4'][i] = key_word_dataframe['key_words'][i][3][0][0]
-            key_word_dataframe['key_word_5'][i] = key_word_dataframe['key_words'][i][4][0][0]
-        except:
-            pass
-    
-    patient_words = pd.merge(left=master_data, right=key_word_dataframe, left_index=True, right_index=True, how='left')
-    patient_words = patient_words[['patient_id', 'key_word_1', 'key_word_2', 'key_word_3', 'key_word_4', 'key_word_5']].drop_duplicates()
-
-    patient_words = patient_words.dropna(subset=['key_word_1', 'key_word_2'])
-
-
-    patient_words['key_word_3'] = patient_words['key_word_3'].fillna(value=pd.np.nan).fillna(" ")
-    patient_words['key_word_4'] = patient_words['key_word_4'].fillna(value=pd.np.nan).fillna(" ")
-    patient_words['key_word_5'] = patient_words['key_word_5'].fillna(value=pd.np.nan).fillna(" ")
-
-
-
-    patients = pd.unique(patient_words['patient_id'])
-    patient_word_bank = pd.DataFrame()
-
-
-    for patient in patients:
-
-        cols = ['key_word_1', 'key_word_2', 'key_word_3', 'key_word_4', 'key_word_5']
-        patient_words['word_bank'] = patient_words[cols].apply(lambda row: ','.join(row.values.astype(str)), axis=1)
-        df = patient_words[['patient_id','word_bank']].drop_duplicates().reset_index()
-        patient_word_bank = pd.concat([patient_word_bank,df])
-    patient_word_bank = patient_word_bank.drop_duplicates()
-
-            
-    patient_word_bank = patient_word_bank.groupby(['patient_id'])['word_bank'].transform(lambda x: ','.join(x)).reset_index().drop_duplicates()
-    #patient_word_bank = pd.DataFrame(patient_word_bank).reset_index()
-
-    for i in range(len(patient_word_bank)):
-        patient_word_bank['word_bank'][i] = patient_word_bank['word_bank'][i].replace(',', ' ')
-        
-    pat_id = patient_words.reset_index()
-    pat_id = pat_id[['patient_id']]
-
-    patient_word_bank = pd.merge(left=patient_word_bank,right=pat_id,right_index=True,left_index=True,how='left')
-
-    patient_word_bank = patient_word_bank[['patient_id', 'word_bank']].drop_duplicates()
-
-    patient_word_bank     
-        
-        
-'''
