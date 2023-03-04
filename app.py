d9a54980eff6a0f7ad9fe56233a643b80f3cb6ba
bennett1993 <bennett.furman@gmail.com>
2023-03-04 01:27:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -209,7 +209,9 @@ with eda:
         
         options = st.multiselect('Would you like to get word banks for specific patients?',word_banks['patient_id'])
         
-        word_banks[word_banks['patient_id'].isin(options),f"{a}_{b}_{c}_{d}_{e}_{f}"]
+        filtered_df = word_banks[word_banks['patient_id'].isin(options)]
+        
+        filtered_df[['patient_id', f"{a}_{b}_{c}_{d}_{e}_{f}"]]
     
         
         
