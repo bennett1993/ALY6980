21f665899657585596b28fac59e0074d9ec5d022
bennett1993 <bennett.furman@gmail.com>
2023-12-02 15:53:26 -0500

    new

--- Home_Page.py
+++ Home_Page.py
@@ -1,36 +1,47 @@
 import pandas as pd
-#pd.set_option('display.max_columns', 500)
-#from pathlib import Path
-#import os
-#import glob
-#from IPython.core.interactiveshell import InteractiveShell
-#InteractiveShell.ast_node_interactivity = "all"
-#import matplotlib.pyplot as plt
-#import seaborn as sns
-#sns.set(font_scale=1.5)
-#import nbconvert
-#import numpy as np
-#import scipy
-#import statsmodels.formula.api as smf
-#import statsmodels.api as sm
+pd.set_option('display.max_columns', 500)
+from pathlib import Path
+import os
+import glob
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = "all"
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(font_scale=1.5)
+import nbconvert
+import numpy as np
+import scipy
+import statsmodels.formula.api as smf
+import statsmodels.api as sm
 import warnings
 warnings.filterwarnings('ignore')
 import streamlit as st
-#from datetime import date as dt
-#import nltk
-#nltk.download('stopwords')
-#nltk.download('wordnet')
-#nltk.download('omw-1.4')
-#from nltk.corpus import stopwords
-#from nltk.stem.wordnet import WordNetLemmatizer
-#import string
-#from keybert import KeyBERT
-#from PIL import Image
+from datetime import date as dt
+import nltk
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+import string
+from keybert import KeyBERT
+from PIL import Image
 
 st.set_page_config(page_title='Home Page', layout="centered")
 st.sidebar.header('Home Page')
 
 header = st.container()
+exec_summary = st.container()
+business_prob = st.container()
+datasets = st.container()
+eda = st.container()
+preprocessing = st.container()
+models_considered = st.container()
+models = st.container()
+recommendations = st.container()
+future_research = st.container()
+other_relevant = st.container()
+citations = st.container()
 
 st.markdown(
     """
@@ -44,11 +55,11 @@ st.markdown(
 )
 
 with header:
-    st.title('ALY 6980 XN Project')
-    st.write('Fledglings Flight')
-    st.write('Bennett Furman, Sam Lurie, Srivaths Ganesh Rao Mahadikar')
+    st.title('ALY 6080 XN Project')
+    st.write('TBI Patient Keyword Extraction')
+    st.write('Aytaj Khankishiyeva, Bennett Furman, Biran Fan, Jacob Vandergriff')
     st.write('Welcome to our Streamlit Application!')
-    st.write('This Streamlit application is linked to this GitHub repository: https://github.com/bennett1993/ALY6980')
+    st.write('This Streamlit application is linked to this GitHub repository: https://github.com/bennett1993/ALY6080')
     
     image = Image.open('image15.png')
     st.image(image)
