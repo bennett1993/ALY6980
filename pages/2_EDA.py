bc894281c651ea91b3529f22b66484a4be9ec872
bennett1993 <bennett.furman@gmail.com>
2023-12-02 15:56:48 -0500

    new

---
+++ pages/2_EDA.py
@@ -0,0 +1,67 @@
+import pandas as pd
+pd.set_option('display.max_columns', 500)
+from pathlib import Path
+import os
+import glob
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = "all"
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(font_scale=1.5)
+import nbconvert
+import numpy as np
+import scipy
+import statsmodels.formula.api as smf
+import statsmodels.api as sm
+import warnings
+warnings.filterwarnings('ignore')
+import streamlit as st
+from datetime import date as dt
+import nltk
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+import string
+from keybert import KeyBERT
+from PIL import Image
+
+st.set_page_config(page_title='EDA', layout="centered")
+st.sidebar.header('EDA')
+
+header = st.container()
+exec_summary = st.container()
+business_prob = st.container()
+datasets = st.container()
+eda = st.container()
+preprocessing = st.container()
+models_considered = st.container()
+models = st.container()
+recommendations = st.container()
+future_research = st.container()
+other_relevant = st.container()
+citations = st.container()
+
+st.markdown(
+    """
+    <style>
+    .main{
+    background-color: #F5F5F5
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+@st.cache_data()
+def get_datasets():
+    DevelopmentCategory = pd.read_csv("https://raw.githubusercontent.com/bennett1993/ALY6980/main/DevelopmentCategory.csv")
+    Milestones = pd.read_csv("https://raw.githubusercontent.com/bennett1993/ALY6980/main/Milestones.csv")
+    AgeGroup = pd.read_csv("https://raw.githubusercontent.com/bennett1993/ALY6980/main/AgeGroup.csv")
+    Exercises = pd.read_csv("https://raw.githubusercontent.com/bennett1993/ALY6980/main/Exercises.csv")
+        
+    return DevelopmentCategory, Milestones, AgeGroup, Exercises
+    
+with datasets:
+    DevelopmentCategory, Milestones, AgeGroup, Exercises = get_datasets()  
